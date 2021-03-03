8aae705332448bed89f6c27d34aef454ca7d16dc
emanuele <emanuele.laterra@studio.unibo.it>
2021-03-03 11:49:02 +0100

    Initial Commit

---
+++ my frist xcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my frist xcode
+//
+//  Created by Emanuele on 03/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
